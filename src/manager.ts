60afcdc804f817cb29d27be9393570248a9418ec
Cenk Kilic <ck@webundsoehne.com>
2020-03-03 12:12:59 +0100

    added option injection to manager.

--- src/manager.ts
+++ src/manager.ts
@@ -1,12 +1,15 @@
 import { ListrContext, ListrOptions, ListrTask } from './interfaces/listr.interface'
-import { ManagerOptions } from './interfaces/manager.interface'
+import { ManagerOptions, ManagerInjectOptions } from './interfaces/manager.interface'
 import { Listr } from './listr'
 
 export class Manager <InjectCtx = ListrContext> {
   // tasks
   private initially: ListrTask[] = []
+  private initialOptions: Exclude<ListrOptions<InjectCtx>, {concurrent}>
   private tasks: ListrTask[] = []
+  private parallelOptions: Exclude<ListrOptions<InjectCtx>, {concurrent}>
   private finally: ListrTask[] = []
+  private finalOptions: Exclude<ListrOptions<InjectCtx>, {concurrent}>
 
   constructor (private options?: ManagerOptions) {
     this.options = Object.assign({ showRunTime: true }, this.options)
@@ -24,6 +27,18 @@ export class Manager <InjectCtx = ListrContext> {
     this.finally = [...this.finally, ...tasks]
   }
 
+  public injectOptions (options: ManagerInjectOptions): void {
+    if (options?.initial) {
+      this.initialOptions = options.initial
+    }
+    if (options?.parallel) {
+      this.parallelOptions = options.parallel
+    }
+    if (options?.final) {
+      this.finalOptions = options.final
+    }
+  }
+
   public async runAll <Ctx = InjectCtx> (options?: Exclude<ListrOptions, {concurrent}>): Promise<Ctx> {
     let pipetime: number
 
@@ -31,13 +46,13 @@ export class Manager <InjectCtx = ListrContext> {
     const managerTasks: ListrTask<Ctx>[] = [
       {
         enabled: (): boolean => this.initially.length > 0,
-        task: (): Listr<Ctx> => new Listr<Ctx>(this.initially)
+        task: (): Listr<Ctx> => new Listr<Ctx>(this.initially, this.initialOptions)
       },
       {
         enabled: (): boolean => this.tasks.length > 0,
         task: (): Listr<Ctx> => {
           pipetime = Date.now()
-          return new Listr<Ctx>(this.tasks, { concurrent: true })
+          return new Listr<Ctx>(this.tasks, { concurrent: true, ...this.parallelOptions })
         }
       },
       {
@@ -46,13 +61,16 @@ export class Manager <InjectCtx = ListrContext> {
       },
       {
         enabled: (): boolean => this.finally.length > 0,
-        task: (): Listr<Ctx> => new Listr<Ctx>(this.finally)
+        task: (): Listr<Ctx> => new Listr<Ctx>(this.finally, this.finalOptions)
       },
       {
         task: (): void => {
           this.initially = []
+          this.initialOptions = {}
           this.tasks = []
+          this.parallelOptions = {}
           this.finally = []
+          this.finalOptions = {}
         }
       }
     ]
@@ -62,7 +80,7 @@ export class Manager <InjectCtx = ListrContext> {
     return this.run<Ctx>(managerTasks, options)
   }
 
-  public run <Ctx> (tasks: ListrTask<Ctx>[], options?: ListrOptions): Promise<Ctx> {
+  public run <Ctx> (tasks: ListrTask<Ctx>[], options?: ListrOptions<Ctx>): Promise<Ctx> {
     return new Listr(tasks, options).run()
   }
 
