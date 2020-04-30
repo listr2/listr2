f5ece36d7c73f1b96728b81f4d88ef07ed24ff58
Cenk Kılıç <cenk@kilic.dev>
2020-04-30 15:44:58 +0200

    build(npmignore): updated npm ignore

--- tests/concurrency.spec.ts
+++ tests/concurrency.spec.ts
@@ -51,57 +51,102 @@ describe('concurrent execution', () => {
 
   it('should run tasks in parallel', async () => {
     await new Listr(tasks, { concurrent: true, renderer: 'test' }).run()
-    const calls = log.mock.calls.flat()
 
     expect(log).toBeCalledTimes(8)
-    expect(calls).toMatchInlineSnapshot(`
+    expect(log.mock.calls).toMatchInlineSnapshot(`
+    Array [
       Array [
         "[STARTED] 1",
+      ],
+      Array [
         "[STARTED] 2",
+      ],
+      Array [
         "[STARTED] 3",
+      ],
+      Array [
         "[STARTED] 4",
+      ],
+      Array [
         "[SUCCESS] 4",
+      ],
+      Array [
         "[SUCCESS] 3",
+      ],
+      Array [
         "[SUCCESS] 2",
+      ],
+      Array [
         "[SUCCESS] 1",
-      ]
+      ],
+    ]
       `)
   })
 
   it('should limit the concurrency', async () => {
     await new Listr(tasks, { concurrent: 2, renderer: 'test' }).run()
-    const calls = log.mock.calls.flat()
 
     expect(log).toBeCalledTimes(8)
-    expect(calls).toMatchInlineSnapshot(`
+    expect(log.mock.calls).toMatchInlineSnapshot(`
     Array [
-      "[STARTED] 1",
-      "[STARTED] 2",
-      "[SUCCESS] 2",
-      "[STARTED] 3",
-      "[SUCCESS] 1",
-      "[STARTED] 4",
-      "[SUCCESS] 4",
-      "[SUCCESS] 3",
+      Array [
+        "[STARTED] 1",
+      ],
+      Array [
+        "[STARTED] 2",
+      ],
+      Array [
+        "[SUCCESS] 2",
+      ],
+      Array [
+        "[STARTED] 3",
+      ],
+      Array [
+        "[SUCCESS] 1",
+      ],
+      Array [
+        "[STARTED] 4",
+      ],
+      Array [
+        "[SUCCESS] 4",
+      ],
+      Array [
+        "[SUCCESS] 3",
+      ],
     ]
     `)
   })
 
   it('should run tasks sequentially', async () => {
     await new Listr(tasks, { concurrent: false, renderer: 'test' }).run()
-    const calls = log.mock.calls.flat()
 
     expect(log).toBeCalledTimes(8)
-    expect(calls).toMatchInlineSnapshot(`
+    expect(log.mock.calls).toMatchInlineSnapshot(`
     Array [
-      "[STARTED] 1",
-      "[SUCCESS] 1",
-      "[STARTED] 2",
-      "[SUCCESS] 2",
-      "[STARTED] 3",
-      "[SUCCESS] 3",
-      "[STARTED] 4",
-      "[SUCCESS] 4",
+      Array [
+        "[STARTED] 1",
+      ],
+      Array [
+        "[SUCCESS] 1",
+      ],
+      Array [
+        "[STARTED] 2",
+      ],
+      Array [
+        "[SUCCESS] 2",
+      ],
+      Array [
+        "[STARTED] 3",
+      ],
+      Array [
+        "[SUCCESS] 3",
+      ],
+      Array [
+        "[STARTED] 4",
+      ],
+      Array [
+        "[SUCCESS] 4",
+      ],
     ]
     `)
   })
