5c7c9caf33eb5141d46cf79cd7176826807b9e50
Cenk Kılıç <cenk@kilic.dev>
2020-07-23 11:19:59 +0200

    fix(tests): change env variable for tests, esspecially chalk because node_env test is more common

--- src/utils/chalk.ts
+++ src/utils/chalk.ts
@@ -2,7 +2,7 @@ import chalkDefault from 'chalk'
 
 let chalk: chalkDefault.Chalk
 
-if (process.env?.NODE_ENV !== 'test' && chalkDefault.supportsColor) {
+if (process.env?.LISTR_DISABLE_COLOR !== '1' && chalkDefault.supportsColor) {
   chalk = new chalkDefault.Instance()
 } else {
   chalk = new chalkDefault.Instance({ level: 0 })
