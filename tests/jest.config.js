45e1070a9804f5eeeeb45ae4bd68a3e5774aee43
Adam <adam@haglund.dev>
2022-01-04 02:45:22 +0100

    lint fix

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   preset: 'ts-jest',
   rootDir: '../',
   testRegex: '(/tests/.*|/src/.*).(e2e-)?spec.tsx?$',
-  setupFiles: [ '<rootDir>/tests/jest.setup.js' ],
+  setupFiles: ['<rootDir>/tests/jest.setup.js'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tests/tsconfig.json'
