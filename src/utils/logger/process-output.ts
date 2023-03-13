474e096346eab10f2deca2347b2e34e89fc284c8
Cenk Kılıç <cenk@kilic.dev>
2023-03-13 15:46:01 +0100

    feat(process-output): ability to overwrite streams on process output

--- src/utils/logger/process-output.ts
+++ src/utils/logger/process-output.ts
@@ -1,11 +1,23 @@
 import { EOL } from 'os'
 
 export class ProcessOutput {
+  private readonly stream: {
+    stdout: NodeJS.WriteStream
+    stderr: NodeJS.WriteStream
+  }
+
+  constructor (stdout: NodeJS.WriteStream = process.stdout, stderr: NodeJS.WriteStream = process.stderr) {
+    this.stream = {
+      stdout,
+      stderr
+    }
+  }
+
   public stdout (buffer: string): boolean {
-    return process.stdout.write(buffer + EOL)
+    return this.stream.stdout.write(buffer + EOL)
   }
 
   public stderr (buffer: string): boolean {
-    return process.stderr.write(buffer + EOL)
+    return this.stream.stderr.write(buffer + EOL)
   }
 }
