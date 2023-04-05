b2f3b952a462a31ab3f53358e80fdaece0b35821
Cenk Kılıç <cenk@kilic.dev>
2023-04-05 20:45:57 +0200

    style: add note

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -3,8 +3,9 @@ import rfdc from 'rfdc'
 const clone = rfdc({ circles: true })
 
 /**
- * Deep clones a object in the most easiest manner.
+ * Deep clones a object in the easiest manner.
  */
+// swap this with structured clone whenever we migrate to node 18
 export function cloneObject<T extends Record<PropertyKey, any>> (obj: T): T {
   return clone(obj)
 }
