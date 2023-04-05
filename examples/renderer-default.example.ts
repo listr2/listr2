2f1e58b05767588d2c09d5ef8acf31f8213c3ed5
Cenk Kılıç <cenk@kilic.dev>
2023-04-05 19:45:44 +0200

    docs: update example

--- examples/renderer-default.example.ts
+++ examples/renderer-default.example.ts
@@ -1,184 +1,187 @@
 import { delay, Listr } from 'listr2'
 
 try {
-  await new Listr([
-    {
-      title: 'Geting you on-board.',
-      task: async (ctx, task): Promise<boolean> =>
-        ctx.user = await task.prompt({
-          type: 'Toggle',
-          message: 'Do you want to create beautiful CLI interfaces?',
-          initial: true
-        })
-    },
+  await new Listr(
+    [
+      {
+        title: 'Geting you on-board.',
+        task: async (ctx, task): Promise<boolean> =>
+          ctx.user = await task.prompt({
+            type: 'Toggle',
+            message: 'Do you want to create beautiful CLI interfaces?',
+            initial: true
+          })
+      },
 
-    {
-      title: 'Doing initial configuration',
-      skip: (ctx): boolean => !ctx.user,
-      task: (_, task): Listr =>
-        task.newListr(
-          [
-            {
-              title: 'Spinning up a new project...',
-              task: async (_, task): Promise<void> => {
-                await delay(2000)
-                task.output = 'Created a new project.'
-              }
-            },
-
-            {
-              title: 'Install Listr2...',
-              task: async (_, task): Promise<void> => {
-                task.output = 'Creating a new repository...'
-                await delay(2000)
-                task.output = 'Installing Listr2...'
-                await delay(2000)
-                task.output = 'Listr2 installed.'
-              }
-            },
-
-            {
-              title: 'Reading the documentation and the examples...',
-              task: async (_, task): Promise<void> => {
-                task.output = 'Documentation can be found in https://listr2.kilic.dev'
-                await delay(500)
-                task.output = 'Created a new instance of Listr!'
-                await delay(500)
-                task.output = 'Added a context.'
-                await delay(500)
-                task.output = 'Added some subtasks.'
-                await delay(500)
-                task.output = 'Get the user input through prompts.'
-                await delay(500)
-                task.output = 'Enabling some tasks.'
-                await delay(500)
-                task.output = 'Skipping some tasks.'
-                await delay(500)
-                task.output = 'Showing some output to user.'
+      {
+        title: 'Doing initial configuration',
+        skip: (ctx): boolean => !ctx.user,
+        task: (_, task): Listr =>
+          task.newListr(
+            [
+              {
+                title: 'Spinning up a new project...',
+                task: async (_, task): Promise<void> => {
+                  await delay(2000)
+                  task.output = 'Created a new project.'
+                }
               },
-              options: {
-                bottomBar: 3
+
+              {
+                title: 'Install Listr2...',
+                task: async (_, task): Promise<void> => {
+                  task.output = 'Creating a new repository...'
+                  await delay(2000)
+                  task.output = 'Installing Listr2...'
+                  await delay(2000)
+                  task.output = 'Listr2 installed.'
+                }
+              },
+
+              {
+                title: 'Reading the documentation and the examples...',
+                task: async (_, task): Promise<void> => {
+                  task.output = 'Documentation can be found in https://listr2.kilic.dev'
+                  await delay(500)
+                  task.output = 'Created a new instance of Listr!'
+                  await delay(500)
+                  task.output = 'Added a context.'
+                  await delay(500)
+                  task.output = 'Added some subtasks.'
+                  await delay(500)
+                  task.output = 'Get the user input through prompts.'
+                  await delay(500)
+                  task.output = 'Enabling some tasks.'
+                  await delay(500)
+                  task.output = 'Skipping some tasks.'
+                  await delay(500)
+                  task.output = 'Showing some output to user.'
+                },
+                options: {
+                  bottomBar: 3
+                }
               }
+            ],
+            { concurrent: true, rendererOptions: { collapse: false } }
+          )
+      },
+
+      {
+        title: 'Create beautiful interfaces...',
+        enabled: (ctx): boolean => ctx.user,
+        task: (_, task): Listr =>
+          task.newListr(
+            [
+              {
+                title: 'Show some output!',
+                task: async (): Promise<void> => {
+                  await delay(2000)
+                }
+              },
+
+              {
+                title: 'Enable tasks conditionally!',
+                task: async (): Promise<void> => {
+                  await delay(1000)
+                }
+              },
+
+              {
+                title: 'Throw and handle some errors!',
+                task: async (_, task): Promise<void> => {
+                  await delay(2500)
+                  task.skip('No errors at all!')
+                }
+              }
+            ],
+            { concurrent: true, rendererOptions: { collapse: false } }
+          )
+      },
+
+      {
+        title: 'Write some code!',
+        enabled: (ctx): boolean => ctx.user,
+        task: (_, task): Listr =>
+          task.newListr(
+            [
+              {
+                title: 'More code...',
+                task: async (): Promise<void> => {
+                  await delay(2000)
+                }
+              },
+
+              {
+                title: 'Oh no the types!',
+                task: async (): Promise<void> => {
+                  await delay(1000)
+                }
+              },
+
+              {
+                title: 'Some tests are needed too!',
+                task: async (): Promise<void> => {
+                  await delay(2500)
+                }
+              },
+
+              {
+                title: 'CI/CD configuration is a must!',
+                task: async (): Promise<void> => {
+                  await delay(2500)
+                }
+              }
+            ],
+            { concurrent: false, rendererOptions: { collapse: true } }
+          )
+      },
+
+      {
+        title: 'Pulling some hair out.',
+        task: (_, task): Listr =>
+          task.newListr(
+            [
+              {
+                title: 'Fixing bugs...',
+                task: async (): Promise<void> => {
+                  await delay(1000)
+                  task.skip('Can not find any bugs!')
+                }
+              },
+
+              {
+                title: 'Fixing type errors...',
+                task: async (): Promise<void> => {
+                  await delay(1000)
+                  throw new Error('This type can not be assigned to that type, oh noes!')
+                }
+              },
+
+              {
+                title: 'Getting some help from Google.',
+                task: async (_, task): Promise<void> => {
+                  await delay(500)
+                  task.title = 'I have found the answers to the universe!'
+                }
+              }
+            ],
+            {
+              concurrent: true,
+              exitOnError: false
             }
-          ],
-          { concurrent: true, rendererOptions: { collapse: false } }
-        )
-    },
+          )
+      },
 
-    {
-      title: 'Create beautiful interfaces...',
-      enabled: (ctx): boolean => ctx.user,
-      task: (_, task): Listr =>
-        task.newListr(
-          [
-            {
-              title: 'Show some output!',
-              task: async (): Promise<void> => {
-                await delay(2000)
-              }
-            },
-
-            {
-              title: 'Enable tasks conditionally!',
-              task: async (): Promise<void> => {
-                await delay(1000)
-              }
-            },
-
-            {
-              title: 'Throw and handle some errors!',
-              task: async (_, task): Promise<void> => {
-                await delay(2500)
-                task.skip('No errors at all!')
-              }
-            }
-          ],
-          { concurrent: true, rendererOptions: { collapse: false } }
-        )
-    },
-
-    {
-      title: 'Write some code!',
-      enabled: (ctx): boolean => ctx.user,
-      task: (_, task): Listr =>
-        task.newListr(
-          [
-            {
-              title: 'More code...',
-              task: async (): Promise<void> => {
-                await delay(2000)
-              }
-            },
-
-            {
-              title: 'Oh no the types!',
-              task: async (): Promise<void> => {
-                await delay(1000)
-              }
-            },
-
-            {
-              title: 'Some tests are needed too!',
-              task: async (): Promise<void> => {
-                await delay(2500)
-              }
-            },
-
-            {
-              title: 'CI/CD configuration is a must!',
-              task: async (): Promise<void> => {
-                await delay(2500)
-              }
-            }
-          ],
-          { concurrent: false, rendererOptions: { collapse: true } }
-        )
-    },
-
-    {
-      title: 'Pulling some hair out.',
-      task: (_, task): Listr =>
-        task.newListr(
-          [
-            {
-              title: 'Fixing bugs...',
-              task: async (): Promise<void> => {
-                await delay(1000)
-                task.skip('Can not find any bugs!')
-              }
-            },
-
-            {
-              title: 'Fixing type errors...',
-              task: async (): Promise<void> => {
-                await delay(1000)
-                throw new Error('This type can not be assigned to that type, oh noes!')
-              }
-            },
-
-            {
-              title: 'Getting some help from Google.',
-              task: async (_, task): Promise<void> => {
-                await delay(500)
-                task.title = 'I have found the answers to the universe!'
-              }
-            }
-          ],
-          {
-            concurrent: true,
-            exitOnError: false
-          }
-        )
-    },
-
-    {
-      title: 'In the end it will all be worth it!',
-      task: async (_, task): Promise<void> => {
-        await delay(1000)
-        task.title = 'Created a beautiful CLI interface!'
+      {
+        title: 'In the end it will all be worth it!',
+        task: async (_, task): Promise<void> => {
+          await delay(1000)
+          task.title = 'Created a beautiful CLI interface!'
+        }
       }
-    }
-  ]).run()
+    ],
+    { renderer: 'default' }
+  ).run()
 } catch (e: any) {
   // eslint-disable-next-line no-console
   console.error(e)
