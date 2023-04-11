148cb813b8d62aa3f5698c733aecda200996fe31
Cenk Kılıç <cenk@kilic.dev>
2023-04-11 18:01:55 +0200

    fix(process-output): make everything optional

--- src/utils/process-output/process-output.ts
+++ src/utils/process-output/process-output.ts
@@ -16,7 +16,7 @@ export class ProcessOutput {
   public readonly stream: ProcessOutputStreamMap
   private active: boolean
 
-  constructor (stdout: NodeJS.WriteStream, stderr: NodeJS.WriteStream, private readonly options?: ProcessOutputOptions) {
+  constructor (stdout?: NodeJS.WriteStream, stderr?: NodeJS.WriteStream, private readonly options?: ProcessOutputOptions) {
     this.stream = {
       stdout: new ProcessOutputStream(stdout ?? process.stdout),
       stderr: new ProcessOutputStream(stderr ?? process.stderr)
