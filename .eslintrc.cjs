5f6405cc9ea800e2849ced1f8e2c02d7db3dbc5e
Cenk Kılıç <cenk@kilic.dev>
2023-09-03 18:05:55 +0200

    style: change eslint behavior with the updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [ '@cenk1cenk2/eslint-config/typescript-dynamic', '@cenk1cenk2/eslint-config/import-strict' ],
+  parserOptions: { project: [ './tsconfig.json', 'docs/tsconfig.json', 'examples/tsconfig.json', 'tests/tsconfig.json' ] },
   rules: {
     ...require('@cenk1cenk2/eslint-config/utils').generateImportGroups({ tsconfigDir: '.' })
   }
