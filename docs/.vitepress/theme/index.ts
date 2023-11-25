2b57e6c849dc715897cf4b6164f215e8dd965e4f
Cenk Kılıç <cenk@kilic.dev>
2023-11-25 16:25:03 +0100

    docs: update fontawesomeicons

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,17 @@
 // https://vitepress.dev/guide/custom-theme
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
 import CodeExampleIcon from './components/CodeExampleIcon.vue'
 import GithubIssue from './components/GithubIssue.vue'
 import Version from './components/Version.vue'
+
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp: ({ app }): void => {
-    app.component('GithubIssue', GithubIssue).component('Version', Version).component('CodeExampleIcon', CodeExampleIcon)
+    app.component('FontAwesomeIcon', FontAwesomeIcon).component('GithubIssue', GithubIssue).component('Version', Version).component('CodeExampleIcon', CodeExampleIcon)
   }
 } satisfies Theme
