78b9a90af37827a060e806cc2b1c6fd031343361
Cenk Kılıç <cenk@kilic.dev>
2024-06-21 12:28:20 +0200

    fix(process-output): trick inquirer and such to not wrap on their own

--- packages/listr2/src/utils/process-output/writable.ts
+++ packages/listr2/src/utils/process-output/writable.ts
@@ -1,8 +1,12 @@
 import { Writable } from 'stream'
+import type { WriteStream } from 'tty'
 
 /* istanbul ignore next */
 export function createWritable (cb: (chunk: string) => void): NodeJS.WritableStream {
   const writable = new Writable()
+
+  ;(writable as WriteStream).rows = Infinity
+  ;(writable as WriteStream).columns = Infinity
 
   writable.write = (chunk: Buffer | string): boolean => {
     cb(chunk.toString())
