a8e94f5f534b85c6a375ed44a4e5e75bae89d309
Yechao Li <liyechao@yechao.xyz>
2025-04-10 15:45:08 +0800

    fix: renderer maybe undefined

--- packages/listr2/src/listr.ts
+++ packages/listr2/src/listr.ts
@@ -223,7 +223,7 @@ export class Listr<
 
     // only the parent task shall exit
     if (this.isRoot()) {
-      this.renderer.end(new Error('Interrupted.'))
+      this.renderer?.end(new Error('Interrupted.'))
 
       process.exit(127)
     }
