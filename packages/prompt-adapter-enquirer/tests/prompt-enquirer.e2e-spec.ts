89ab0846d2a3673bbfd338f2154ca437369b1647
Cenk Kılıç <cenk@kilic.dev>
2025-12-23 21:42:31 +0100

    test: let us skip the test if even enquirer gets an update

--- packages/prompt-adapter-enquirer/tests/prompt-enquirer.e2e-spec.ts
+++ packages/prompt-adapter-enquirer/tests/prompt-enquirer.e2e-spec.ts
@@ -96,10 +96,7 @@ describe.each<RendererSetup>(RENDERER_SETUP)('%s renderer: prompt -> enquirer', 
   })
 
   // HaNF6mQO2FvhOQe4bPQRBRhk9MS9lx9w
-  // HACK: Skip on Node.js 24+ due to incompatibility with enquirer 2.4.1
-  // enquirer has not been updated since 2022 and has issues with Node.js 24's readline changes
-  // See: https://github.com/nodejs/node/issues/53497, https://github.com/nodejs/node/issues/57678
-  ;(parseInt(process.versions.node.split('.')[0], 10) >= 24 ? it.skip : it)('should run cancel callback', async() => {
+  it.skip('should run cancel callback', async() => {
     const ctx = new Listr(
       [
         {
