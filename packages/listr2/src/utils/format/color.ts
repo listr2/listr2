dcc955ba70a71428b2dc13141fd61ed278f82e31
hyperz111 <firzeroun11@gmail.com>
2026-03-01 04:50:52 +0700

    fix: fix types

--- packages/listr2/src/utils/format/color.ts
+++ packages/listr2/src/utils/format/color.ts
@@ -1,8 +1,9 @@
-import * as util from 'node:util'
+import { inspect, styleText } from 'node:util'
+import type { InspectColor } from 'node:util'
 
 /**
  * Color palette.
  */
-export const color: { [k in keyof typeof util.inspect.colors]: (text: string | number) => string } = Object.fromEntries(
-  Object.keys(util.inspect.colors).map((color) => [color, (text: string | number): string => util.styleText(color, String(text))])
-)
+export const color = Object.fromEntries(Object.keys(inspect.colors).map((color) => [color, (text) => styleText(color as InspectColor, String(text))])) as {
+  [K in InspectColor]: (text: string | number) => string
+}
