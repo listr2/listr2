43c570289a4cfcf6504d66949226b332d2f97e3c
hyperz111 <firzeroun11@gmail.com>
2026-03-01 19:53:17 +0700

    fix: fix lint issues

--- packages/listr2/src/utils/format/color.ts
+++ packages/listr2/src/utils/format/color.ts
@@ -4,6 +4,4 @@ import type { InspectColor } from 'node:util'
 /**
  * Color palette.
  */
-export const color = Object.fromEntries(Object.keys(inspect.colors).map((color) => [color, (text) => styleText(color as InspectColor, String(text))])) as {
-  [K in InspectColor]: (text: string | number) => string
-}
+export const color = Object.fromEntries(Object.keys(inspect.colors).map((color) => [color, (text: string | number): string => styleText(color as InspectColor, String(text))])) as Record<InspectColor, (text: string | number) => string>
