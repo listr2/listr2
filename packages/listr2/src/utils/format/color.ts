113a40966afa6c427374aa2908b3eaf2b1c82d9e
hyperz111 <firzeroun11@gmail.com>
2026-03-01 19:56:55 +0700

    format: control length

--- packages/listr2/src/utils/format/color.ts
+++ packages/listr2/src/utils/format/color.ts
@@ -4,4 +4,6 @@ import type { InspectColor } from 'node:util'
 /**
  * Color palette.
  */
-export const color = Object.fromEntries(Object.keys(inspect.colors).map((color) => [color, (text: string | number): string => styleText(color as InspectColor, String(text))])) as Record<InspectColor, (text: string | number) => string>
+export const color = Object.fromEntries(
+  Object.keys(inspect.colors).map((color) => [color, (text: string | number): string => styleText(color as InspectColor, String(text))])
+) as Record<InspectColor, (text: string | number) => string>
